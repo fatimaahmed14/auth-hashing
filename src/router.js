4359f02f255d4efe0619799554e6b4ca9d3028b2
fatima <fxtimaahmed14@gmail.com>
2023-01-24 11:34:41 +0000

    req 1 complete

--- src/router.js
+++ src/router.js
@@ -7,11 +7,18 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    
+    const { username, password } = req.body
+
+    const saltRound = 10
+    const salt = bcrypt.genSaltSync(saltRound)
+
+    const hash = bcrypt.hashSync(password, salt)
+
+    res.json({ username, passsword: hash })
 });
 
 router.post('/login', async (req, res) => {
-    
+
 });
 
 module.exports = router;
